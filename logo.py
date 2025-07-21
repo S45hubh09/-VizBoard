513544c08089e11d4ef36db0bdb0f4e72f9e9907
S45hubh09 <shubhamgharage1@gmail.com>
2025-07-21 21:10:13 +0530

    Update logo.py

--- logo.py
+++ logo.py
@@ -1,1 +1,6 @@
-logo
+import streamlit as st
+from PIL import Image
+
+# Load and display the logo from the 'assets' folder
+logo = Image.open("assets/logo.jpg")  # or logo.png
+st.image(logo, width=145)
